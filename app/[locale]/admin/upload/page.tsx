86c3a60713b6fa23ed7b822eab9e9095e4d7cefd
stipica19 <pipiklepic1@gmail.com>
2026-04-11 19:26:24 +0200

    fix: add force-dynamic to admin pages to prevent prerender errors

--- app/[locale]/admin/upload/page.tsx
+++ app/[locale]/admin/upload/page.tsx
@@ -1,15 +1,7 @@
-"use client";
-import UploadImage from "@/components/UploadImage";
+import UploadClient from "./UploadClient";
+
+export const dynamic = "force-dynamic";
 
 export default function UploadPage() {
-  const handleUploaded = (_url: string) => {
-    // callback za UploadImage komponentu
-  };
-
-  return (
-    <div className="p-4">
-      <h1 className="text-2xl font-bold mb-4">Upload Slika</h1>
-      <UploadImage onUploaded={handleUploaded} />
-    </div>
-  );
+  return <UploadClient />;
 }
